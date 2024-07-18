e207a81c6ea9a232ec0290b36c7bfa48766ee471
boyko1396 <boyko1396@gmail.com>
2024-07-18 20:15:03 +0300

    test page

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * !(i)
+ * The code is included in the final file only when a function is called, for example: FLSFunctions.spollers();
+ * Or when the entire file is imported, for example: import "files/script.js";
+ * Unused code does not end up in the final file.
+
+ * If we want to add a module, we should uncomment it.
+ */
+
+// support webp, identify device
+import BaseHelpers from './helpers/BaseHelpers.js';
+
+BaseHelpers.checkWebpSupport();
+BaseHelpers.addTouchClass();
+BaseHelpers.addLoadedClass();
+
+document.addEventListener('DOMContentLoaded', function() {
+});
